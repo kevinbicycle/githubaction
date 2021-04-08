40a5890f7bf514d13c079e908f13abede748b0ec
kevinbicycle <liangxinxin001@163.com>
2021-04-08 08:15:39 +0800

    update test

--- actiontest/MainWindow.xaml.cs
+++ actiontest/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace actiontest
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
